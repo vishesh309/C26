2aa4ee6e3aec0c39c460476f611da5735b8b7968
vishesh309 <visheshhathiya0@gmail.com>
2021-04-26 19:53:33 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    plateform=new Ground(200,300,400,320)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -48,7 +48,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    plateform.display();
     box3.display();
     box4.display();
     pig3.display();
